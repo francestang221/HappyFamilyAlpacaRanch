07680896194c1ec6aaa7656d9f88197cd0be14f1
Frances Tang <francest221@gmail.com>
2021-04-15 01:44:15 -0700

    Update database connection

--- source code/database/dbcon.js
+++ source code/database/dbcon.js
@@ -3,10 +3,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_tangmm',
-  password        : '7680',
-  database        : 'cs340_tangmm'
+  host            : 'us-cdbr-east-03.cleardb.com',
+  user            : 'bcb9fa44832f0f',
+  password        : 'dd68b3b3',
+  database        : 'Happy Family Alpaca Ranch'
 });
 
 module.exports.pool = pool;
